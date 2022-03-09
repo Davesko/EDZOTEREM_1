857e094442125d021f7b524aa5454d92608ecb2c
Davesko <nagyhazu.david@gmail.com>
2022-03-09 12:31:52 +0100

    2022.03.09 javitás 2.0

---
+++ header.php
@@ -0,0 +1,75 @@
+<head>
+
+    <meta charset="utf-8">
+
+
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+
+    <title >Fitness Vital</title>
+
+
+    <link rel="icon" href="assets/images/logo.jpg" type="image/icon type">
+
+
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="assets/css/templatemo-training-studio.css">
+
+    <link rel="stylesheet" href="assets/css/style.css.css">
+
+</head>
+
+<body>
+
+<!-- ***** Preloader Start ***** -->
+<div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+        <span class="dot"></span>
+        <div class="dots">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+</div>
+<!-- ***** Preloader End ***** -->
+
+<!-- ***** Header Area Start ***** -->
+<header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="#" class="logo">FITNESS<em> VITAL</em></a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li class="scroll-to-section"><a href="#top" class="active">KEZDŐLAP</a></li>
+                        <li class="scroll-to-section"><a href="#prices">ÁRAK</a></li>
+                        <li class="scroll-to-section"><a href="#our-classes">ÓRÁK</a></li>
+                        <li class="scroll-to-section"><a href="#schedule">MENETREND</a></li>
+                        <li class="scroll-to-section"><a href="#trainers">EDZŐINK</a></li>
+                        <li class="scroll-to-section"><a href="#contact-us">KONTAKT</a></li>
+                        <li class="main-button"><a href="register.php">REGISZTRÁCIÓ</a></li>
+                    </ul>
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- ***** Header Area End ***** -->
+
+
+
+
+
+
+
--- index.php
+++ index.php
@@ -1,73 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
-
-    <title>Training Studio - Free CSS Template</title>
-
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-
-    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
-
-   <link rel="stylesheet" href="assets/css/templatemo-training-studio.css">
-
-    <link rel="stylesheet" href="assets/css/style.css.css">
-
-    </head>
-    
-    <body>
-    
-    <!-- ***** Preloader Start ***** -->
-    <div id="js-preloader" class="js-preloader">
-      <div class="preloader-inner">
-        <span class="dot"></span>
-        <div class="dots">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-    </div>
-    <!-- ***** Preloader End ***** -->
-    
-    
-    <!-- ***** Header Area Start ***** -->
-    <header class="header-area header-sticky">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <nav class="main-nav">
-                        <!-- ***** Logo Start ***** -->
-                        <a href="index.html" class="logo">FITNESS<em> VITAL</em></a>
-                        <!-- ***** Logo End ***** -->
-                        <!-- ***** Menu Start ***** -->
-                        <ul class="nav">
-                            <li class="scroll-to-section"><a href="#top" class="active">KEZDŐLAP</a></li>
-                            <li class="scroll-to-section"><a href="#prices">ÁRAK</a></li>
-                            <li class="scroll-to-section"><a href="#our-classes">ÓRÁK</a></li>
-                            <li class="scroll-to-section"><a href="#schedule">MENETREND</a></li>
-                            <li class="scroll-to-section"><a href="#trainers">EDZŐINK</a></li>
-                            <li class="scroll-to-section"><a href="#contact-us">KONTAKT</a></li>
-                            <li class="main-button"><a href="#">REGISZTRÁCIÓ</a></li>
-                        </ul>        
-                        <a class='menu-trigger'>
-                            <span>Menu</span>
-                        </a>
-                        <!-- ***** Menu End ***** -->
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </header>
-    <!-- ***** Header Area End ***** -->
+    <?php include "header.php" ?>
 
 
+<body>
 
     <!-- ***** Main Banner Area Start ***** -->
     <div class="main-banner" id="top">
@@ -105,6 +42,7 @@
                 </div>
             </div>
         </div>
+    </section>
 
   <div class="container">
     <table class="table justify-content-center">
@@ -129,13 +67,19 @@
         <th scope="row">10 alkalom</th>
         <td>11.000 Forint</td>
         <td>9.000 Forint</td>
-        <td>60 nap</td>
+        <td>30 nap</td>
     </tr>
     <tr>
         <th scope="row">20 alkalom</th>
         <td>20.000 Forint</td>
         <td>16.000 Forint</td>
-        <td>90 nap</td>
+        <td>60 nap</td>
+    </tr>
+    <tr>
+        <th scope="row">1 havi</th>
+        <td>11.000 Forint</td>
+        <td>8.000 Forint</td>
+        <td>30 nap</td>
     </tr>
     <tr>
         <th scope="row">1 év</th>
@@ -426,39 +370,10 @@
     </section>
     <!-- ***** Contact Us Area Ends ***** -->
     
-    <!-- ***** Footer Start ***** -->
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; 2022 FITNESS VITAL
+<?php include "footer.php"?>
+
+</body>
 
 
-                    
 
-                    
-                </div>
-            </div>
-        </div>
-    </footer>
-
-    <!-- jQuery -->
-    <script src="assets/js/jquery-2.1.0.min.js"></script>
-
-    <!-- Bootstrap -->
-    <script src="assets/js/popper.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-
-    <!-- Plugins -->
-    <script src="assets/js/scrollreveal.min.js"></script>
-    <script src="assets/js/waypoints.min.js"></script>
-    <script src="assets/js/jquery.counterup.min.js"></script>
-    <script src="assets/js/imgfix.min.js"></script> 
-    <script src="assets/js/mixitup.js"></script> 
-    <script src="assets/js/accordions.js"></script>
-    
-    <!-- Global Init -->
-    <script src="assets/js/custom.js"></script>
-
-  </body>
 </html>
